95cb27ad371771a7e6b778bbc8bb363322bc6b8d
Sam Parry <samparrymusic@gmail.com>
2020-11-26 14:33:27 +0000

    workoing on list checkbox

--- src/App.js
+++ src/App.js
@@ -1,27 +1,56 @@
-
+import React from 'react'
 import './App.css';
 import Header from './Header'
 import List from './List'
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <List items={tasks} />
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    tasks: [
+      {
+        title: 'Make an App',
+        urgency: 'URGENT',
+        status: '🔲'
+      },
+      {
+        title: 'Get Rich',
+        urgency: 'IMPORTANT',
+        status: '🔲'
+      },
+      {
+        title: 'Move to Spain',
+        urgency: 'NOT NECESSARY YET',
+        status: '🔲'
+      }
+    ]
+  }
+
+  checkBox = (index) => {
+    console.log(this.state.tasks[index].status)
+    if (this.state.tasks[index].status === '🔲') {
+      this.setState((currentState) => {
+        const newTasks = [...currentState.tasks];
+        const newState = {
+          [newTasks[index]]: '✅'
+        }
+        console.log(newState)
+        return newState
+      })
+    } else {
+      return this.state.tasks[index].status === '🔲'
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <List checkBox={this.checkBox}
+          items={this.state.tasks} />
+      </div>
+    );
+  }
 }
 
-const tasks = [
-  {
-    title: 'Make an App'
-  },
-  {
-    title: 'Get Rich'
-  },
-  {
-    title: 'Move to Spain'
-  }
-]
 
 export default App;
--- src/List.js
+++ src/List.js
@@ -1,19 +1,23 @@
 import './App'
 
 const list = (props) => {
+
     return (
         <div>
             <ul>
-                {props.items.map(item => {
+                {props.items.map((item, index) => {
                     return (
                         <li key={item.title}>
                             <p>{item.title}</p>
+                            <p onClick={() => props.checkBox(index)}>{item.status}</p>
+                            <p>Urgency: {item.urgency}</p>
                         </li>
                     )
                 })}
             </ul>
         </div>
     )
+
 }
 
 export default list;
