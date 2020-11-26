f808ec292427357a505fc1c4c80c00f054911643
Sam Parry <samparrymusic@gmail.com>
2020-11-26 12:11:19 +0000

    initialised app

---
+++ Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App'
+
+const Header = (props) => {
+    return (
+        <header>
+            <h1>Sam and Liam's To Do List</h1>
+        </header>
+    )
+}
+
+export default Header;
